67008e59afc6bb89c4739da39d1c31d5f3da7858
TechnicalCoffeeLogic <trevorconvirs@gmail.com>
2022-11-05 22:24:53 -0500

    Added hero and nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bailey: {
+					50: '#A6882E',
+					100: '#735E20',
+				}
+			}
+		},
 	},
 	plugins: [],
 }
